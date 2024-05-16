98f6ceb4834e74470212283f1d7f21fa8592466d
Tommy47x <cristianleulbdt@gmail.com>
2024-05-16 11:29:16 +0300

    version 0.02

--- elections/models.py
+++ elections/models.py
@@ -4,6 +4,9 @@ class Candidate(models.Model):
     name = models.CharField(max_length=200)
     party = models.CharField(max_length=200)
     image_url = models.URLField(max_length=200)
+    votes = models.IntegerField(default=0)  # new field
+    description = models.TextField(default ='')  # new field
+    party_image_url = models.URLField(max_length=200, default ='')  # new field
 
     def __str__(self):
         return self.name
